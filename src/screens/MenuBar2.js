85ab213126c858be2181e074e78fcedab528ee98
Sai-Charan-Bandari <bsaicharan30@gmail.com>
2023-01-21 00:53:06 +0530

    updated Modal in MenuBar2

--- src/screens/MenuBar2.js
+++ src/screens/MenuBar2.js
@@ -1,24 +1,47 @@
-import { StyleSheet, Text, View,Image,TouchableOpacity ,Modal,TextInput, ScrollView} from 'react-native'
+import { StyleSheet, Text, View,Image,TouchableOpacity ,Modal,TextInput, ScrollView,FlatList} from 'react-native'
 import {useNavigation} from '@react-navigation/native'
 import React, { useState } from 'react'
 
 const SearchModal=({setShowSearch})=>{
     return(
-        <View>
+        // animationType in Modal tells how the transitions take place when creating or destroying the Modal
+        <Modal animationType="slide" transparent={true} >
+            <View style={{backgroundColor:"white"}}>
         <View style={styles.container}>
             <TouchableOpacity onPress={()=>setShowSearch(false)}>
             <Image style={{width:22,height:22,margin:15,borderRadius:10,marginRight:10}} source={{uri: "https://cdn-icons-png.flaticon.com/128/2976/2976252.png"}} />
             </TouchableOpacity>
-        <TextInput style={{width:'80%'}} placeholder='Search for skills subjects, or software'></TextInput>
+        <TextInput  style={{width:'80%'}} placeholder='Search for skills subjects, or software'></TextInput>
         </View>
-        <ScrollView>
-            <Text style={{fontWeight:'500'}}>Recent searches</Text>
-            <View>
-            <Text>Recent searches</Text>
-
-            </View>
+        <ScrollView style={{height:300}}>
+            <Text style={{fontWeight:'500',margin:10}}>Recent searches</Text>
+            {['career development','communication','react-native'].map((ele,index)=>
+            <TouchableOpacity style={{display:'flex',flexDirection:'row',margin:5}} key={index}>
+            <Image style={{width:20,height:20,marginHorizontal:10}} source={{uri: "https://cdn-icons-png.flaticon.com/128/2088/2088617.png"}} />
+            <Text>{ele}</Text>
+            </TouchableOpacity>)}
+                <Text style={{fontWeight:'500',margin:10}}>Certification Offerings</Text>
+                <FlatList style={{marginLeft:10}} showsHorizontalScrollIndicator={false} horizontal data={['Certification Preparation','Continuing Education','Academic Credits']} 
+                renderItem={(ele)=>
+                    <TouchableOpacity style={styles.rectangle}>
+                        <Text >
+                            {ele.item}
+                        </Text>
+                    </TouchableOpacity>
+                }></FlatList>
+                <Text style={{fontWeight:'500',margin:10}}>I'm looking for...</Text>
+                <FlatList style={{marginLeft:10}} showsHorizontalScrollIndicator={false} horizontal data={['Courses','Learning Paths','Audio','Events','Videos']}
+                renderItem={(ele)=>
+                    <TouchableOpacity style={styles.ellipse}>
+                    <Text >
+                        {ele.item}
+                    </Text>
+                        
+                    </TouchableOpacity>
+                }></FlatList>
         </ScrollView>
-        </View>
+            </View>
+        </Modal>
     )
 }
 
@@ -57,5 +80,22 @@ const styles = StyleSheet.create({
         width:400,
         borderWidth:1,
         borderColor:'black',
+    },
+    rectangle:{
+        padding:12,
+        borderRadius:5,
+        borderWidth:1,
+        borderColor:'black',
+        marginHorizontal:5,
+        width:150,
+        alignItems:'center'
+    },
+    ellipse:{
+        padding:6,
+        borderRadius:40,
+        borderWidth:1,
+        borderColor:'black',
+        marginHorizontal:5
+
     }
 })
